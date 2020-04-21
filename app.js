a595427dfe9d599301284fc0f73e63a6138802cd
Mikl-LG <legallicmickael@inspekt.fr>
2020-04-21 13:12:19 +0200

    create machine, create bookings, read machine_catalog

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const agrilocationRoutes = require('./routes/agrilocation.js');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
@@ -82,10 +83,130 @@ const upload = multer({
 
 //app.use('/',agrilocationRoutes);
 
-app.post('/post/addMachineImage',upload.array('filedata'),function(req,res){
-  console.log('reqfiles : ',req.files);
-  console.log('reqbody : ',req.body);
-  res.status(200).send(req.files);
+app.get('/get/machine',
+  (req, res, next)=>{
+    s3.getObject(     
+      {
+        Bucket:'inspekt-prod',
+        Key:'AGRILOCATION/machine_catalog', //2 paramètres obligatoires pour toute méthode s3 (/ inplicite entre Bucket et Key)
+      },
+      //Prévoir un check de l'existence du bucket avec (data&&data.Body&data.Body.toString()) || [] 
+      //erreur de key => s3 renvoi noSuchKey
+      //voir BucketActions dans le backend d'Inspekt
+      (error,data)=>{
+          //console.log('data : ',JSON.parse(data.Body.toString()))
+          res.status(
+            error ? 400 : 201
+          ).send(
+            {data:JSON.parse(data.Body.toString())}
+          )      
+      })
+  });
+
+app.post('/post/addBooking', async(req, res, next)=>{
+    
+  const newBooking = req.body;
+
+  /**GET ALL THE MACHINE CATALOG */
+    s3.getObject(
+      {
+        Bucket:'inspekt-prod',
+        Key:'AGRILOCATION/machine_catalog',
+      },
+      (error,data)=>{
+        let MACHINE_CATALOG = JSON.parse(data.Body.toString());
+        const targetMachine = MACHINE_CATALOG.filter(element=>element.id == newBooking.idMachine);
+        
+        try{
+          let bookingMerged = targetMachine[0].booking?[...targetMachine[0].booking]:[];
+          bookingMerged.push(newBooking);
+
+          /**UPDATE MACHINE OF MACHINE_CATALOG WHICH IS CONCERNED BY THE NEW BOOKING */
+          const MACHINE_CATALOG_UPDATED = MACHINE_CATALOG.map((machine)=>{
+
+            if(machine.id == newBooking.idMachine){
+
+              let machineUpdated = machine;
+              machineUpdated.booking = bookingMerged;
+              return (machineUpdated);
+
+            }else{
+
+              return (machine);
+
+            } 
+          })
+  /**UPLAOD ALL THE MACHINE CATALOG UPDATED */
+          s3.putObject(
+            {
+              Bucket:'inspekt-prod',
+              Key:'AGRILOCATION/machine_catalog', //2 paramètres obligatoires pour toute méthode s3 (/ inplicite entre Bucket et Key)
+              Body:JSON.stringify(MACHINE_CATALOG_UPDATED) // uniquement nécessaire pour les requêtes POST
+            },
+            (error,response)=>{
+                res.status(
+                  error ? 400 : 201
+                ).json(
+                  {error,response}
+                )
+            }
+          )
+
+        }catch(error){
+          console.log('erreur : ',error);
+        }
+      }
+    )   
+})
+
+app.post('/post/addMachineImage',upload.array('filedata'),(req,res)=>{
+
+  s3.getObject(
+    {
+      Bucket:'inspekt-prod',
+      Key:'AGRILOCATION/machine_catalog',
+    },
+    async(error,data)=>{
+      let MACHINE_CATALOG = await JSON.parse(data.Body.toString());
+      let newCatalog = [...MACHINE_CATALOG];
+
+      const uuid = uuidv4();
+
+      let machine = req.body;
+      machine.id = uuid;
+      machine.status = 'active';
+      if(req.files[0]){
+        machine.image_url = req.files[0].location;
+      }else{
+        machine.image_url = 'noImageUrl'
+      }
+      
+      newCatalog.push(machine);
+      
+      console.log('newCatalog : ',newCatalog);
+      if(machine.brand=='undefined' || machine.brand=='' || machine.nature=='undefined' || machine.nature=='' ||machine.type=='undefined' || machine.type=='' || machine.day_price=='undefined' || machine.day_price=='' || machine.hour_price=='undefined' || machine.hour_price==''){
+        res.status(400).send('noData');
+      }else{
+        console.log('saving into database',machine.brand);
+        s3.putObject(
+          {
+            Bucket:'inspekt-prod',
+            Key:'AGRILOCATION/machine_catalog', //2 paramètres obligatoires pour toute méthode s3 (/ inplicite entre Bucket et Key)
+            Body:JSON.stringify(newCatalog) // uniquement nécessaire pour les requêtes POST
+          },
+          (error,response)=>{
+              res.status(
+                error ? 400 : 201
+              ).json(
+                {error,response}
+              )
+            
+          })
+      }
+      
+      })
+  
+  //res.status(200).send(req.files);
 });
 
 app.listen(4001);
